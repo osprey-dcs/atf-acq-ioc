8ab7dd9f1266c5e8ff51bd6dd43796de19975c6e
Eric Norum <wenorum@lbl.gov>
2023-09-25 16:38:39 -0700

    Begin adding hooks for multiple acquisition nodes. OPI screens still need to be converted.

--- iocBoot/siocNASA_ACQ/st.cmd
+++ iocBoot/siocNASA_ACQ/st.cmd
@@ -2,9 +2,8 @@
 
 ###############################################################################
 # Set up environment
-epicsEnvSet(NASA_ACQ_FPGA_IP, "$(NASA_ACQ_FPGA_IP=192.168.19.7)")
-epicsEnvSet(P, "$(NASA_ACQ_P=NASAACQ:)")
-epicsEnvSet(R, "$(NASA_ACQ_R=0:)")
+epicsEnvSet(NASA_ACQ_FPGA_IP, "$(NASA_ACQ_FPGA_IP=192.168.19.1)")
+epicsEnvSet(P, "$(NASA_ACQ_P=NASA_ACQ:)")
 < envPaths
 cd "$(TOP)"
 
@@ -15,19 +14,22 @@ nasaAcq_registerRecordDeviceDriver pdbbase
 
 ###############################################################################
 # Set up connections
-createPSCUDP("NASA_CTRL", "$(NASA_ACQ_FPGA_IP)", 54398, 1)
-createPSCUDPFast("NASA_ACQ", "$(NASA_ACQ_FPGA_IP)", 54399, 0)
+createPSCUDP("NASA_CTRL00", "$(NASA_ACQ_FPGA_IP)00", 54398, 1)
+# And repeat 32 times, one for each node
+createPSCUDPFast("NASA_ACQ00", "$(NASA_ACQ_FPGA_IP)00", 54399, 0)
+# And repeat 32 times, one for each node
 var PSCDebug 0
 var PSCUDPDSyncSizeMB 20
 
 ###############################################################################
 # Load record instances
-dbLoadRecords("db/nasaAcqSup.db", "P=$(P),R=$(R),PORT=NASA_CTRL")
-dbLoadRecords("db/pscudpfast.db", "P=$(P)$(R),NAME=NASA_ACQ")
+dbLoadRecords("db/nasaAcqSup.db", "P=$(P),PORT=NASA_CTRL")
+# FIXME: This has to be redone to handle multiple acquisition nodes -- force use of the first node for now.
+dbLoadRecords("db/pscudpfast.db", "P=$(P),NAME=NASA_ACQ00")
 
 ###############################################################################
 # Start IOC
 cd "$(TOP)/iocBoot/$(IOC)"
 iocInit
-dbpf "$(P)$(R)FileDir-SP" "."
-dbpf "$(P)$(R)FileBase-SP" "FOO"
+dbpf "$(P)FileDir-SP" "."
+dbpf "$(P)FileBase-SP" "FOO"
--- opi/runIt.sh
+++ opi/runIt.sh
@@ -3,8 +3,8 @@
 export JAVA_HOME="/Applications/CSS_Phoebus.app/jdk"
 PATH="$JAVA_HOME/Contents/Home/bin:$PATH"
 
-export P="NASAACQ:"
-export R="0:"
+export P="NASA_ACQ:"
+export R="00:"
 
 export arch="$EPICS_HOST_ARCH"
 /Applications/CSS_Phoebus.app/phoebus-4.6.10-SNAPSHOT/phoebus.sh -resource "file://$PWD/nasaACQ_sysmon.bob?app=display_runtime"
