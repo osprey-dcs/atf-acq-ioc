55f5cdaa55e3975271b391ff6189315448b2b417
Eric Norum <wenorum@lbl.gov>
2023-09-25 17:14:13 -0700

    More hooks for multiple acquisition nodes.

--- opi/runIt.sh
+++ opi/runIt.sh
@@ -4,7 +4,6 @@ export JAVA_HOME="/Applications/CSS_Phoebus.app/jdk"
 PATH="$JAVA_HOME/Contents/Home/bin:$PATH"
 
 export P="NASA_ACQ:"
-export R="00:"
 
 export arch="$EPICS_HOST_ARCH"
-/Applications/CSS_Phoebus.app/phoebus-4.6.10-SNAPSHOT/phoebus.sh -resource "file://$PWD/nasaACQ_sysmon.bob?app=display_runtime"
+/Applications/CSS_Phoebus.app/phoebus-4.6.10-SNAPSHOT/phoebus.sh -resource "file://$PWD/nasaACQ.bob?app=display_runtime"
