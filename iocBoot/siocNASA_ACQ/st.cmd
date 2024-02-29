95f5f917a60ba184da003cbb37c75f34918275bb
Eric Norum <wenorum@lbl.gov>
2024-02-28 16:31:04 -0800

    Move initialization commands to 'temporary' command file.  Invoke on a per-node basis.

--- iocBoot/siocNASA_ACQ/st.cmd
+++ iocBoot/siocNASA_ACQ/st.cmd
@@ -20,6 +20,7 @@ cd "$(TOP)/iocBoot/$(IOC)"
 
 # Repeat the following 32 times -- one for each node
 iocshLoad "config_node.cmd" "NODE=01"
+iocshLoad "config_node.cmd" "NODE=02"
 
 ###############################################################################
 cd "$(TOP)"
@@ -36,70 +37,10 @@ iocInit
 dbpf "$(P)01:FileDir-SP" "/tmp"
 dbpf "$(P)01:FileBase-SP" "FOO-"
 dbpf "$(P)01:Record-Sel" 1
+dbpf "$(P)02:FileDir-SP" "/tmp"
+dbpf "$(P)02:FileBase-SP" "BAR-"
+dbpf "$(P)02:Record-Sel" 1
 
-dbpf "$(P)01:ACQ:coupling:01" "DC"
-dbpf "$(P)01:ACQ:coupling:02" "DC"
-dbpf "$(P)01:ACQ:coupling:03" "DC"
-dbpf "$(P)01:ACQ:coupling:04" "DC"
-dbpf "$(P)01:ACQ:coupling:05" "DC"
-dbpf "$(P)01:ACQ:coupling:06" "DC"
-dbpf "$(P)01:ACQ:coupling:07" "DC"
-dbpf "$(P)01:ACQ:coupling:08" "DC"
-dbpf "$(P)01:ACQ:coupling:09" "DC"
-dbpf "$(P)01:ACQ:coupling:10" "DC"
-dbpf "$(P)01:ACQ:coupling:11" "DC"
-dbpf "$(P)01:ACQ:coupling:12" "DC"
-dbpf "$(P)01:ACQ:coupling:13" "DC"
-dbpf "$(P)01:ACQ:coupling:14" "DC"
-dbpf "$(P)01:ACQ:coupling:15" "DC"
-dbpf "$(P)01:ACQ:coupling:16" "DC"
-dbpf "$(P)01:ACQ:coupling:17" "DC"
-dbpf "$(P)01:ACQ:coupling:18" "DC"
-dbpf "$(P)01:ACQ:coupling:19" "DC"
-dbpf "$(P)01:ACQ:coupling:20" "DC"
-dbpf "$(P)01:ACQ:coupling:21" "DC"
-dbpf "$(P)01:ACQ:coupling:22" "DC"
-dbpf "$(P)01:ACQ:coupling:23" "DC"
-dbpf "$(P)01:ACQ:coupling:24" "DC"
-dbpf "$(P)01:ACQ:coupling:25" "DC"
-dbpf "$(P)01:ACQ:coupling:26" "DC"
-dbpf "$(P)01:ACQ:coupling:27" "DC"
-dbpf "$(P)01:ACQ:coupling:28" "DC"
-dbpf "$(P)01:ACQ:coupling:29" "DC"
-dbpf "$(P)01:ACQ:coupling:30" "DC"
-dbpf "$(P)01:ACQ:coupling:31" "DC"
-dbpf "$(P)01:ACQ:coupling:32" "DC"
-
-dbpf "$(P)01:ACQ:enable:01" 1
-dbpf "$(P)01:ACQ:enable:02" 1
-dbpf "$(P)01:ACQ:enable:03" 1
-dbpf "$(P)01:ACQ:enable:04" 1
-dbpf "$(P)01:ACQ:enable:05" 1
-dbpf "$(P)01:ACQ:enable:06" 1
-dbpf "$(P)01:ACQ:enable:07" 1
-dbpf "$(P)01:ACQ:enable:08" 1
-dbpf "$(P)01:ACQ:enable:09" 1
-dbpf "$(P)01:ACQ:enable:10" 1
-dbpf "$(P)01:ACQ:enable:11" 1
-dbpf "$(P)01:ACQ:enable:12" 1
-dbpf "$(P)01:ACQ:enable:13" 1
-dbpf "$(P)01:ACQ:enable:14" 1
-dbpf "$(P)01:ACQ:enable:15" 1
-dbpf "$(P)01:ACQ:enable:16" 1
-dbpf "$(P)01:ACQ:enable:17" 1
-dbpf "$(P)01:ACQ:enable:18" 1
-dbpf "$(P)01:ACQ:enable:19" 1
-dbpf "$(P)01:ACQ:enable:20" 1
-dbpf "$(P)01:ACQ:enable:21" 1
-dbpf "$(P)01:ACQ:enable:22" 1
-dbpf "$(P)01:ACQ:enable:23" 1
-dbpf "$(P)01:ACQ:enable:24" 1
-dbpf "$(P)01:ACQ:enable:25" 1
-dbpf "$(P)01:ACQ:enable:26" 1
-dbpf "$(P)01:ACQ:enable:27" 1
-dbpf "$(P)01:ACQ:enable:28" 1
-dbpf "$(P)01:ACQ:enable:29" 1
-dbpf "$(P)01:ACQ:enable:30" 1
-dbpf "$(P)01:ACQ:enable:31" 1
-dbpf "$(P)01:ACQ:enable:32" 1
-
+# This should be autosave/restore or some startup client
+iocshLoad "initig_node.cmd" "NODE=01"
+iocshLoad "initig_node.cmd" "NODE=02"
