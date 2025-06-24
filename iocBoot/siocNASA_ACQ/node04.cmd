422b35a002956313c184490af58bb5a634342e0b
Michael Davidsaver <mdavidsaver@gmail.com>
2025-06-24 13:54:10 -0700

    IP address changes from april

--- iocBoot/siocNASA_ACQ/node04.cmd
+++ iocBoot/siocNASA_ACQ/node04.cmd
@@ -1,6 +1,6 @@
 #!../../bin/linux-x86_64/nasaAcq
 
-epicsEnvSet(NASA_ACQ_BASE_IP, "192.168.79.9")
+epicsEnvSet(NASA_ACQ_BASE_IP, "192.168.79.112")
 epicsEnvSet(NODE, "04")
 
 < nodeXX.cmd
--- iocBoot/siocNASA_ACQ/node27.cmd
+++ iocBoot/siocNASA_ACQ/node27.cmd
@@ -1,6 +1,6 @@
 #!../../bin/linux-x86_64/nasaAcq
 
-epicsEnvSet(NASA_ACQ_BASE_IP, "192.168.79.32")
+epicsEnvSet(NASA_ACQ_BASE_IP, "192.168.79.102")
 epicsEnvSet(NODE, "27")
 
 < nodeXX.cmd
